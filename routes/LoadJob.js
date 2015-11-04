4f12946dddda8d86ecf29a4ac9f024a22f733181
i5ting <i5ting@126.com>
2015-11-04 23:31:09 +0800

    fix name err

--- routes/LoadJob.js
+++ routes/LoadJob.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('loadJob.', {
-    title: 'loadJob.'
+  res.render('LoadJob', {
+    title: 'loadJob'
   });
 });
 
