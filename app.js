ffde13de71e7afc78a6d79d39e29daca38de1ce8
i5ting <i5ting@126.com>
2015-11-04 23:52:23 +0800

    code format

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var log4js        = require('log4js');
+var log4js = require('log4js');
 
-var app           = express();
-var log           = log4js.getLogger("moa-api");
+var app = express();
+var log = log4js.getLogger("moa-api");
 
 var mount = require('mount-routes');
 
 // simple
 // mount(app);
-mount(app,__dirname + '/routes', true);
+mount(app, __dirname + '/routes', false);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -37,7 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -48,7 +48,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -59,7 +59,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
--- gulpfile.js
+++ gulpfile.js
@@ -5,31 +5,31 @@ var mount = require('mount-routes');
 
 var source_path = ['test/**/*.js', 'lib/*.js'];
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
   gulp.watch(source_path, ['mocha']);
 });
 
 gulp.task('mocha', function () {
-    return gulp.src(source_path , {read: false})
-        // gulp-mocha needs filepaths so you can't have any plugins before it 
-        .pipe(mocha({reporter: 'spec'}));
+  return gulp.src(source_path, { read: false })
+  // gulp-mocha needs filepaths so you can't have any plugins before it 
+    .pipe(mocha({ reporter: 'spec' }));
 });
 
-gulp.task('routes', function() {
-  var express       = require('express');
-  var app           = express();
+gulp.task('routes', function () {
+  var express = require('express');
+  var app = express();
   
   // mount routes
   mount(app, __dirname + '/routes', true);
 });
 
-gulp.task('kp', function() {
+gulp.task('kp', function () {
   var kp = require("kp");
   var is_sudo = false;
-  var pre = is_sudo == true ? 'sudo' : '' ;
-  
+  var pre = is_sudo == true ? 'sudo' : '';
+
   kp(3010, pre);
 });
 
 
-gulp.task('default',['mocha', 'watch']);
+gulp.task('default', ['mocha', 'watch']);
