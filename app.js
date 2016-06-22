73322f50f6c85ef2dc7599ef00ebf86b4c4fe116
AprilClj <1271678029@qq.com>
2016-06-22 15:05:19 +0800

    bootstraptable

--- app.js
+++ app.js
@@ -19,8 +19,9 @@ var simditor_qn_config = require('./config/simditor_qn')
 
 // mount 
 mount_simditor_qn(app, simditor_qn_config);
-mount_routes(app, __dirname + '/routes', false);
+
 mount_uploadify(app,{
+  // qn: simditor_qn_config.qn,//是否开启上传到七牛
   path:'/fileupload',
   fileKey:'myfile',
   multer: simditor_qn_config.multer
@@ -44,6 +45,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', routes);
 // app.use('/users', users);
+mount_routes(app, __dirname + '/routes', false);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
